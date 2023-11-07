feab349dbd9009e6531032db25452490aa4a9fb6
awais512 <raza.awais25@gmail.com>
2023-11-07 01:08:39 +0000

    Showing Categories and selecting categories

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,18 @@
-const SearchPage = () => {
-  return <div>This is a search page</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/Categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;
