d296da78e8a8075d68c29f03e24102666576b18f
awais512 <raza.awais25@gmail.com>
2023-11-09 21:35:16 +0000

    Course Mobile Sidebar

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,5 +1,6 @@
 import NavbarRoutes from "@/components/navbar-routes";
 import { Chapter, Course, UserProgress } from "@prisma/client";
+import CourseMobileSidebar from "./course-mobile-sidebar";
 
 interface CourseNavbarProps {
   course: Course & {
@@ -13,6 +14,7 @@ interface CourseNavbarProps {
 const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
   return (
     <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <CourseMobileSidebar course={course} progressCount={progressCount} />
       <NavbarRoutes />
     </div>
   );
