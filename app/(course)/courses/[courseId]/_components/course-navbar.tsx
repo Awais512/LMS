1495cb10216345dd97bb44444db74bd171a7563f
awais512 <raza.awais25@gmail.com>
2023-11-09 02:26:47 +0000

    Course Navbar

--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -1,5 +1,21 @@
-const CourseNavbar = () => {
-  return <div>Course Navbar</div>;
+import NavbarRoutes from "@/components/navbar-routes";
+import { Chapter, Course, UserProgress } from "@prisma/client";
+
+interface CourseNavbarProps {
+  course: Course & {
+    chapters: (Chapter & {
+      userProgress: UserProgress[] | null;
+    })[];
+  };
+  progressCount: number;
+}
+
+const CourseNavbar = ({ course, progressCount }: CourseNavbarProps) => {
+  return (
+    <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+      <NavbarRoutes />
+    </div>
+  );
 };
 
 export default CourseNavbar;
--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -51,9 +51,9 @@ const CourseLayout = async ({
 
   return (
     <div className="h-full">
-      {/* <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
+      <div className="h-[80px] md:pl-80 fixed inset-y-0 w-full z-50">
         <CourseNavbar course={course} progressCount={progressCount} />
-      </div> */}
+      </div>
       <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
         <CourseSidebar course={course} progressCount={progressCount} />
       </div>
