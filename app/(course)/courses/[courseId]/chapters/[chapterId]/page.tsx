7781503083d54054c8e406d208cf6c6bc05c44ee
awais512 <raza.awais25@gmail.com>
2023-11-11 10:12:34 +0000

    Conditionally rendering based on chapters type free or paid

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -0,0 +1,53 @@
+import { getChapter } from "@/actions/get-chapter";
+import Banner from "@/components/banner";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+  params,
+}: {
+  params: { courseId: string; chapterId: string };
+}) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const {
+    chapter,
+    course,
+    muxData,
+    attachments,
+    nextChapter,
+    userProgress,
+    purchase,
+  } = await getChapter({
+    userId,
+    chapterId: params.chapterId,
+    courseId: params.courseId,
+  });
+
+  if (!chapter || !course) {
+    return redirect("/");
+  }
+
+  const isLocked = !chapter.isFree && !purchase;
+  const completeOnEnd = !!purchase && !userProgress?.isCompleted;
+
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+    </div>
+  );
+};
+
+export default ChapterIdPage;
