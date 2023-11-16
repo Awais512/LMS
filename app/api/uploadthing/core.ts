3058fb5c5830319946a515503e2c4b72883ec76d
awais512 <raza.awais25@gmail.com>
2023-11-16 12:22:41 +0000

    Protecting our frontend and Backend

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,17 +1,18 @@
 import { auth } from "@clerk/nextjs";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 
+import { isTeacher } from "@/lib/teacher";
+
 const f = createUploadthing();
 
 const handleAuth = () => {
   const { userId } = auth();
-  if (!userId) {
-    throw new Error("Unauthorized");
-  }
+  const isAuthorized = isTeacher(userId);
+
+  if (!userId || !isAuthorized) throw new Error("Unauthorized");
   return { userId };
 };
 
-// FileRouter for your app, can contain multiple FileRoutes
 export const ourFileRouter = {
   courseImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
     .middleware(() => handleAuth())
--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useAuth } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import SearchInput from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 const NavbarRoutes = () => {
+  const { userId } = useAuth();
+
   const pathname = usePathname();
 
   const isTeacherPage = pathname?.startsWith("/teacher");
@@ -29,13 +32,13 @@ const NavbarRoutes = () => {
               Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId!) ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher Mode
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
