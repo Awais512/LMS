6946b0b82b90d536a71da0fc81915649d55cc256
awais512 <raza.awais25@gmail.com>
2023-11-16 11:23:38 +0000

    DataCard

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -1,5 +1,24 @@
-const AnalyticsPage = () => {
-  return <div>Analytics PAge</div>;
+import { getAnalytics } from "@/actions/getAnalytics";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { DataCard } from "./_components/data-card";
+
+const AnalyticsPage = async () => {
+  const { userId } = auth();
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { data, totalRevenue, totalSales } = await getAnalytics(userId);
+
+  return (
+    <div className="p-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
+        <DataCard label="Total Revenue" value={totalRevenue} shouldFormat />
+        <DataCard label="Total Sales" value={totalSales} />
+      </div>
+    </div>
+  );
 };
 
 export default AnalyticsPage;
